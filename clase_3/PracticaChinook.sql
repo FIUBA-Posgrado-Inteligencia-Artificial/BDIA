1f841302b291294bcb47757e64a849567e93ccaa
OP211673 <pablo.octaviano@icbc.com.ar>
2023-09-09 10:03:03 -0300

    clase 3

---
+++ clase_3/PracticaChinook.sql
@@ -0,0 +1,39 @@
+-- Selecciona todos los registros de la tabla Albums.
+
+-- Selecciona todos los géneros únicos de la tabla Genres.
+
+-- Cuenta el número de pistas por género.
+
+-- Encuentra la longitud total (en milisegundos) de todas las pistas para cada álbum.
+
+-- Lista los 10 álbumes con más pistas.
+
+-- Encuentra la longitud promedio de la pista para cada género.
+
+-- Para cada cliente, encuentra la cantidad total que han gastado.
+
+-- Para cada país, encuentra la cantidad total gastada por los clientes.
+
+-- Clasifica a los clientes en cada país por la cantidad total que han gastado.
+
+-- Para cada artista, encuentra el álbum con más pistas y clasifica a los artistas por este número.
+
+-- Selecciona todas las pistas que tienen la palabra "love" en su título.
+
+-- Selecciona a todos los clientes cuyo primer nombre comienza con 'A'.
+
+-- Calcula el porcentaje del total de la factura que representa cada factura.
+
+-- Calcula el porcentaje de pistas que representa cada género.
+
+-- Para cada cliente, compara su gasto total con el del cliente que gastó más.
+
+-- Para cada factura, calcula la diferencia en el gasto total entre ella y la factura anterior.
+
+-- Para cada factura, calcula la diferencia en el gasto total entre ella y la próxima factura.
+
+-- Encuentra al artista con el mayor número de pistas para cada género.
+
+-- Compara el total de la última factura de cada cliente con el total de su factura anterior.
+
+-- Encuentra cuántas pistas de más de 3 minutos tiene cada álbum.
